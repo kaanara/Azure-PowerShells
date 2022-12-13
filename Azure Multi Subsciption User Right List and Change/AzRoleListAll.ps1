41309fbe42f7d7e2d4d4c648a4367ceadace3ae3
Kaan ARA <kaanara@gmail.com>
2022-12-13 11:15:22 +0300

    Create AzRoleListAll.ps1

---
+++ Azure Multi Subsciption User Right List and Change/AzRoleListAll.ps1
@@ -0,0 +1,20 @@
+﻿#Connect-AzAccount
+
+$filename = (Get-date).Day.ToString() + (Get-date).Month.ToString() + (Get-date).Year.ToString()
+$filename = "RGNew" + $filename + ".csv"
+
+if(Test-Path $filename) {
+    Remove-Item $filename -Force
+}
+
+$subscriptions = Get-AzSubscription
+
+ForEach($sub in $subscriptions)
+{
+    Select-AzSubscription -SubscriptionId $sub.SubscriptionId
+    $group = Get-AzResourceGroup
+    ForEach($rg in $group){
+        #Get-AzRoleAssignment -ResourceGroupName $rg.ResourceGroupName | select-Object @{l="SubscriptionName";e={$sub.Name}}, @{l="ResourceGroupName";e={$rg.ResourceGroupName}},RoleDefinitionName,Scope,ObjectType,DisplayName,SignInName | Export-Csv $filename -NoTypeInformation -Append -Force
+        $Roles= Get-AzRoleAssignment -ResourceGroupName $rg.ResourceGroupName | select-Object @{l="SubscriptionName";e={$sub.Name}}, @{l="ResourceGroupName";e={$rg.ResourceGroupName}},RoleDefinitionName,Scope,ObjectType,DisplayName,SignInName 
+    }
+}
