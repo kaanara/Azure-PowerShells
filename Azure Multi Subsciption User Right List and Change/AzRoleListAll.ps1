589390787cbd66276c98b296b56fe8dcf86fbc05
Kaan ARA <kaanara@gmail.com>
2022-12-15 15:39:50 +0300

    Update AzRoleListAll.ps1

--- Azure Multi Subsciption User Right List and Change/AzRoleListAll.ps1
+++ Azure Multi Subsciption User Right List and Change/AzRoleListAll.ps1
@@ -9,12 +9,11 @@ if(Test-Path $filename) {
 
 $subscriptions = Get-AzSubscription
 
-ForEach($sub in $subscriptions)
+ForEach($sub in $subscriptions) 
 {
     Select-AzSubscription -SubscriptionId $sub.SubscriptionId
     $group = Get-AzResourceGroup
     ForEach($rg in $group){
-        #Get-AzRoleAssignment -ResourceGroupName $rg.ResourceGroupName | select-Object @{l="SubscriptionName";e={$sub.Name}}, @{l="ResourceGroupName";e={$rg.ResourceGroupName}},RoleDefinitionName,Scope,ObjectType,DisplayName,SignInName | Export-Csv $filename -NoTypeInformation -Append -Force
-        $Roles= Get-AzRoleAssignment -ResourceGroupName $rg.ResourceGroupName | select-Object @{l="SubscriptionName";e={$sub.Name}}, @{l="ResourceGroupName";e={$rg.ResourceGroupName}},RoleDefinitionName,Scope,ObjectType,DisplayName,SignInName 
+        Get-AzRoleAssignment -ResourceGroupName $rg.ResourceGroupName | select-Object @{l="SubscriptionName";e={$sub.Name}}, @{l="ResourceGroupName";e={$rg.ResourceGroupName}},RoleDefinitionName,Scope,ObjectType,DisplayName,SignInName | Export-Csv $filename -NoTypeInformation -Append -Force
     }
 }
